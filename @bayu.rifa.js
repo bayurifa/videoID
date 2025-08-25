ec049b00839663ef273e4a7866b9e867d2e3ca0e
bayurifa <91443870+bayurifa@users.noreply.github.com>
2025-08-26 03:47:17 +0700

    @bayu.rifa.js

---
+++ @bayu.rifa.js
@@ -0,0 +1,10 @@
+<script>
+  // Data video ID dan URL video
+  const videoData = {
+    "bayu": "https://github.com/bayurifa/videoID/raw/refs/heads/main/Jumpscare.mp4",
+
+    "cantik": "https://github.com/bayurifa/videoID/raw/refs/heads/main/Jumpscare.mp4",
+
+    "BAYU": "https://github.com/bayurifa/videoID/raw/refs/heads/main/Jumpscare.mp4"
+  };
+</script>
